3daea6874b4a39ecf486e2f334b5fbd542de4e73
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-10-31 10:02:44 +0300

    [OVERONE-HW] Homework #6

--- Kiryl_2022/todolist/urls.py
+++ Kiryl_2022/todolist/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('index/', index, name='index'),
-    path('about-us/', about_us, name='about_us'),
+    path('about/', about_us, name='about_us'),
 ]
