a700ab47a20914f7b75548ffa30499512933a6f9
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-11-05 16:36:51 +0300

    Add paginator, add the page with a list of all requests with the ability to delete any request.

--- Kiryl_2022/weather_app/urls.py
+++ Kiryl_2022/weather_app/urls.py
@@ -4,4 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', MainPage.as_view(), name='index'),
+    path('requests/', RequestsList.as_view(), name='requests'),
+    path('requests/delete/<int:pk>', RequestDelete.as_view(), name='request_delete'),
 ]
--- Kiryl_2022/weather_app/views.py
+++ Kiryl_2022/weather_app/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DeleteView
 
 from weather_app.forms import CityForm
 from weather_app.models import City
@@ -13,7 +13,6 @@ class MainPage(CreateView):
     form_class = CityForm
     template_name = 'weather_app/index.html'
     success_url = reverse_lazy('index')
-    context_object_name = 'cities'
 
     def get_context_data(self, **kwargs):
         context = super(MainPage, self).get_context_data(**kwargs)
@@ -27,3 +26,20 @@ class MainPage(CreateView):
         form.instance.time = info['time']
         form.save()
         return super().form_valid(form)
+
+
+class RequestsList(ListView):
+    model = City
+    paginate_by = 4
+    template_name = 'weather_app/requests.html'
+    context_object_name = 'cities'
+
+
+class RequestDelete(DeleteView):
+    model = City
+
+    def get_success_url(self):
+        if self.request.POST.get('requests-delete-from-list'):
+            return reverse_lazy('requests')
+        else:
+            return reverse_lazy('index')
