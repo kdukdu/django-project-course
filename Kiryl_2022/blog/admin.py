13769cd302b5dd9f68dfb9d4c0999337ec171920
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-11-09 23:09:44 +0300

    Some changes

--- Kiryl_2022/blog/admin.py
+++ Kiryl_2022/blog/admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
 
-from .models import Comment, Post, Category
+from .models import Comment, Post
+
 
 # Register your models here.
-admin.site.register(Category)
-
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created_on', 'modified_on']
-    filter_horizontal = ['categories']
-    ordering = ['title']
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['author', 'created_on', 'post']
-    ordering = ['author']
+    list_display = ('author', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
