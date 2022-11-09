3831a23f85e27f37a594d148bcb107145e10f697
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-11-09 23:08:55 +0300

    Changed models

--- Kiryl_2022/blog/forms.py
+++ Kiryl_2022/blog/forms.py
@@ -1,17 +1,18 @@
 from django import forms
 
+from blog.models import Comment
 
-class CommentForm(forms.Form):
-    author = forms.CharField(
-        max_length=60,
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Ваше имя'
-        })
-    )
-    body = forms.CharField(
-        widget=forms.Textarea(attrs={
-            'class': 'form-control',
-            'placeholder': 'Ваш комментарий'
-        })
-    )
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('author', 'email', 'body')
+        labels = {
+            'body': 'Comment',
+            'author': 'Name'
+        }
+        widgets = {
+            'author': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', "cols": "40", "rows": "4"})
+        }
--- Kiryl_2022/blog/models.py
+++ Kiryl_2022/blog/models.py
@@ -1,39 +1,50 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
-
-
-# Create your models here.
-
-
-class Category(models.Model):
-    title = models.CharField(max_length=30)
-
-    def __str__(self):
-        return f'{self.title}'
-
-    def get_posts(self):
-        return reverse('blog-category', args=[self.title])
+from django.utils import timezone
+from taggit.managers import TaggableManager
 
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+
     title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    modified_on = models.DateTimeField(auto_now=True)
-    categories = models.ManyToManyField(Category, related_name='posts')
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    tags = TaggableManager()
 
     def __str__(self):
-        return f'{self.title}'
+        return self.title
 
-    def get_url(self):
-        return reverse('blog-detail', args=[self.id])
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.publish.year,
+                                            self.publish.strftime('%m'),
+                                            self.publish.strftime('%d'),
+                                            self.slug])
+
+    class Meta:
+        ordering = ('-publish',)
 
 
 class Comment(models.Model):
-    author = models.CharField(max_length=50)
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=50)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.body}'
+
+    class Meta:
+        ordering = ('created',)
