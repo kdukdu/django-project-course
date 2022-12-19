2fc6127c45b29548575010f176a480e224fd0b6c
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-12-19 23:00:38 +0300

    Fix: Fix error with choosing type of model's field

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.OneToOneField('CustomUser', related_name='posts', on_delete=models.CASCADE)
+    author = models.ForeignKey('CustomUser', related_name='posts', on_delete=models.CASCADE)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
